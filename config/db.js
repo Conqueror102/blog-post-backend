5477457d3824ce14b82ff994fdd72c8f69dd4dd8
victor conqueror <victorvector608@gmail.com>
2025-03-12 12:40:59 +0100

    features:url

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const { connect } = require("mongoose");
+const { mongoose } = require("mongoose");
 
 require("dotenv/config");
 
@@ -7,7 +7,7 @@ const {MONGODB_URL} = process.env
 const connectdb = async ()=>{
     try{
         await
-        connect(MONGODB_URL?.toString());
+        mongoose.connect("mongodb+srv://victorConqueror:2293201518@cluster0.ktmou.mongodb.net/BILL");
         console.log("server connected successfully");
     }catch(err){
         console.log("unable to connect",err.message);
